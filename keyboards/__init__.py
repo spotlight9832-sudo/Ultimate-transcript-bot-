db8b040e9343800f2b6b612acd9d99ea1f0f4900
spotlight9832-sudo <spotlight9832@gmail.com>
2026-06-27 12:17:34 +0530

    Create __init__.py

---
+++ keyboards/__init__.py
@@ -0,0 +1,16 @@
+"""keyboards package"""
+from .inline import (
+    language_keyboard,
+    output_format_keyboard,
+    approve_reject_keyboard,
+    cancel_keyboard,
+    translate_language_keyboard,
+)
+
+__all__ = [
+    "language_keyboard",
+    "output_format_keyboard",
+    "approve_reject_keyboard",
+    "cancel_keyboard",
+    "translate_language_keyboard",
+]
